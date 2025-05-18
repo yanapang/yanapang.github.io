7d3cc38e2fb7a591e53b70c45909a6c98a7744a1
yana <yanahwang@gmail.com>
2025-05-18 13:21:43 +0900

    env: bootstrap setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ Bootstrap 전역 적용
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
