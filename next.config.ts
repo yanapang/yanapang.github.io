25ea6a848ffe651abb8affb0f2f41347af9bba47
yanapang <yanahwang@gmail.com>
2025-08-13 01:44:25 +0900

    test: pipeline modification

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+// GitHub 저장소 이름 (username.github.io 레포면 ''로 둡니다)
+const repoName = 'my-next-project';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
