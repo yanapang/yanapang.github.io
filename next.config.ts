912e8c8a520e7c6418942f8a2fd29bf2ac63f8d7
yanapang <yanahwang@gmail.com>
2025-08-13 01:55:44 +0900

    fix: modification applied

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 // GitHub 저장소 이름 (username.github.io 레포면 ''로 둡니다)
-const repoName = 'my-next-project';
+const repoName = 'yanapang.github.io';
 
 const nextConfig: NextConfig = {
   output: 'export',
