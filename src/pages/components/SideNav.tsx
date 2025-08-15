74dd5f1a6aeef34ec93ac358e97a1e06978c694a
yehwang <ye.hwang@okestro.com>
2025-08-15 19:50:01 +0900

    style: fix margins

--- src/pages/components/SideNav.tsx
+++ src/pages/components/SideNav.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function SideNav() {
   return (
     <ul className="nav flex-column">
-      <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" href="/">Home</Link>
-      </li>
+      {/*<li className="nav-item">*/}
+      {/*  <Link className="nav-link active" aria-current="page" href="/">Home</Link>*/}
+      {/*</li>*/}
       {/*<li className="nav-item">*/}
       {/*  <a className="nav-link" href="#">Blog</a>*/}
       {/*</li>*/}
